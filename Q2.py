bfd61e6ec697c6d8b9c2ca4c490cd473338eddf0
vivekster12 <vivek.gidla@mail.mcgill.ca>
2019-05-09 12:59:37 -0400

    refactored rule2 validity check into sep method

--- Q2.py
+++ Q2.py
@@ -1,34 +1,74 @@
 import re
 
-def method(string1, string2):
-    # first, strings must start with X.Y.Z
-    standard = "^[1-9][0-9]*\.[0-9]+\.[0-9]+"
-    # if string doesn't have a match then its an invalid string
-    std_list = []
-    for string in (string1,string2):
-        match = re.search(standard,string)
-        if not match:
-            print (string + " is invalid check 1")
-            return 
-        std_list.append(match.group())
-    
+def rule2(string1, string2):
+    # rule 2 of semantic version (2.0.0)
+    # A normal version number MUST take the form X.Y.Z where X, Y, and Z are non-negative integers, and MUST NOT contain leading zeroes. 
+    # X is the major version, Y is the minor version, and Z is the patch version. Each element MUST increase numerically.
+    # method returns 1 if string 1 is in invalid format, 2 if string 2 is in invalid format. if both are valid, returns the X.Y.Z part of both strings 
+
+    standard = "^[1-9][0-9]*\.[0-9]+\.[0-9]+" # if string doesn't have a match then its an invalid string
+    std_list = [] # store the X.Y.Z version string here (if it is valid)
+    match1 = re.search(standard,string1)
+    if not match1:
+        return 1
+    match2 = re.search(standard,string2)
+    if not match2:
+        return 2
+    std_list.append(match1.group())
+    std_list.append(match2.group())
+
     # second, make sure none of Y,Z don't have a leading zero (already checked X does not lead with a 0)
-    for string in std_list:
+    for i,string in enumerate(std_list):
         spl_str = string.split('.')
         for val in spl_str:
             if len(val)>1 and val[0] == '0':
-                print (string + " is invalid check 2")
+                if i == 0:
+                    return 1
+                else:
+                    return 2
+    return std_list
 
-    # now, compare standard version strings (if difference found here, can avoid looking at rest of strings
-    std_prec = standard_precedence(std_list[0],std_list[1])
+
+#def rule9(string1,string2):
+
+
+def method(string1, string2):
+#    # rule 2 of semantic version (2.0.0)
+#    # A normal version number MUST take the form X.Y.Z where X, Y, and Z are non-negative integers, and MUST NOT contain leading zeroes. X is the major version, Y is the minor version, and Z is the patch version. Each element MUST increase numerically. 
+#    # first, strings must start with X.Y.Z
+#    standard = "^[1-9][0-9]*\.[0-9]+\.[0-9]+"
+#    # if string doesn't have a match then its an invalid string
+#    std_list = []
+#    for string in (string1,string2):
+#        match = re.search(standard,string)
+#        if not match:
+#            print (string + " is invalid check 1")
+#            return 
+#        std_list.append(match.group())
+#    
+#    # second, make sure none of Y,Z don't have a leading zero (already checked X does not lead with a 0)
+#    for string in std_list:
+#        spl_str = string.split('.')
+#        for val in spl_str:
+#            if len(val)>1 and val[0] == '0':
+#                print (string + " is invalid check 2")
+
+    std_list = rule2(string1,string2)
+
+    # now, compare X.Y.Z  version strings (if difference found here, can avoid looking at rest of strings)
+    std_prec = maj_min_patch__precedence(std_list[0],std_list[1])
     if std_prec > 0:
         print (string1, " is greater")
         return
     elif std_prec < 0:
         print (string2, " is greater")
     else:
-        # check if strings have "pre-release" (denoted by '-')
+        # rule 9: A pre-release version MAY be denoted by appending a hyphen and a series of dot separated identifiers immediately following the patch version. 
+        # Identifiers MUST comprise only ASCII alphanumerics and hyphen [0-9A-Za-z-]. Identifiers MUST NOT be empty. Numeric identifiers MUST NOT include leading zeroes.
+        # need to refactor rule9 validity check!
         if '-' in string1 and '-' in string2:
+            # both strings have pre-rel info
+
             #print ("need to do pre-version precedence checks")
             pre_rel = []
             for string in (string1,string2):
@@ -103,7 +143,9 @@ def alphaCheck(input1, input2):
     else:
         return 1
 
-def standard_precedence(input1,input2):
+def maj_min_patch_precedence(input1,input2):
+    # Precedence is determined by the first difference when comparing each of these identifiers from left to right as follows: Major, minor, and patch versions are always compared numerically.
+    # method returns 1 if string 1 is greater, -1 if string 2 is greater, 0 if both equal
     for str1,str2 in zip(input1.split('.'),input2.split('.')):
         if int(str1)>int(str2):
             return 1
