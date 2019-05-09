5c839645a9ce091bde303095ce1207a6abf38e13
vivekster12 <vivek.gidla@mail.mcgill.ca>
2019-05-09 12:18:04 -0400

    question 1 of test

---
+++ overlapLines.py
@@ -0,0 +1,18 @@
+def method(in1, in2):
+    # identify the line that starts before
+    if in1[0] < in2[0]:
+        # input 1 starts before input 2
+        line1 = in1
+        line2 = in2
+    elif in1[0] > in2[0]:
+        line1 = in2
+        line2 = in1
+    else:
+        # both lines have same start point ... overlap
+        return True
+
+    if line1[1] >= line2[0]:
+        # start of end of input1 comes after or at start of input2. ... overlap
+            return True
+        else:
+            return False
